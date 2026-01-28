a8f1547b8c1d20cc7edab30e5ddf1e473e3e94f1
Kaustubhshinde11 <kaustubhshinde1104@gmail.com>
2026-01-28 14:07:00 +0530

    Fix add.py for GitHub Actions

--- add.py
+++ add.py
@@ -1,35 +1,26 @@
-# Python program to display the Fibonacci sequence
+from math import sqrt
 
 def recur_fibo(n):
-   if n <= 1:
-       return n
-   else:
-       return(recur_fibo(n-1) + recur_fibo(n-2))
+    if n <= 1:
+        return n
+    return recur_fibo(n-1) + recur_fibo(n-2)
 
-nterms = 10
+def main():
+    nterms = 10
 
-# check if the number of terms is valid
-if nterms <= 0:
-   print("Plese enter a positive integer")
-else:
-   print("Fibonacci sequence:")
-   for i in range(nterms):
-       print(recur_fibo(i))
+    if nterms <= 0:
+        print("Please enter a positive integer")
+    else:
+        print("Fibonacci sequence:")
+        for i in range(nterms):
+            print(recur_fibo(i))
 
+    # formula-based fibonacci
+    def nthFib(n):
+        res = ((1+sqrt(5))**n - (1-sqrt(5))**n) / (2**n * sqrt(5))
+        print(int(res), "is", n, "th fibonacci number")
 
+    nthFib(10)
 
-
-
-# To find the n-th Fibonacci Number using formula
-from math import sqrt 
-# import square-root method from math library
-def nthFib(n):
-    res = (((1+sqrt(5))**n)-((1-sqrt(5)))**n)/(2**n*sqrt(5))
-    # compute the n-th fibonacci number
-    print(int(res),'is',str(n)+'th fibonacci number')
-    # format and print the number
-    
-# driver code
-nthFib(12)
-
-# This code is contributed by Kush Mehta
+if __name__ == "__main__":
+    main()
